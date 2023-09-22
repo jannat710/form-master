bf46e957e4b0d7cde828a6729b91e46e1792c3e4
e <jannatul0040@gmail.com>
2023-09-22 09:55:27 +0600

    added hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import RefForm from './components/RefForm/RefForm'
+import StateFulForm from './components/StateFulForm/StateFulForm'
+// import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
 
   return (
     <>
       <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StateFulForm></StateFulForm> */}
+      <RefForm></RefForm>
     </>
   )
 }
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,23 @@
 
 
 const SimpleForm = () => {
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        console.log('form submitted');
+    }
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name"/>
                 <br />
-                <button>Submit</button>
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="text" name="phone"/>
+                <br />
+                <input type="submit" value="Submit"/>
             </form>
         </div>
     );
